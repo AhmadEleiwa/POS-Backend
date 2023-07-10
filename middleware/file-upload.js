94c82faaea60dfc31902f2bb0caa2d28d4d1d168
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-10 18:37:31 +0300

    Create file-upload utils for handling the storing images insdie upload folder

---
+++ middleware/file-upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+const { v4: uuid } = require('uuid');
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+};
+
+const imageUpload = multer({
+  limits: 50000000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
+    }
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error('Invalid mime type!');
+    cb(error, isValid);
+  }
+});
+
+
+
+exports.imageUpload = imageUpload;
+
