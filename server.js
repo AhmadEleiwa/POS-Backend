e7a4f008f9cc24aea19892f087fd2e5bc66873dd
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-12 12:12:45 +0300

    Adding the api route

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const todoRouter = require("./routes/todo-route");
 const { default: mongoose } = require("mongoose");
 const categoryRoute = require("./routes/catgeory-route");
 const unitOfMeasureRoute = require("./routes/unitOfmeasure-route");
 const productRoute = require("./routes/product-route");
+const userRoute = require("./routes/user-route");
+const cartRoute = require("./routes/cart-route");
+
+
 
 
 let dotenv = require("dotenv").config();
 
 const app = express();
-
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -25,12 +27,19 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use('/uploads', express.static('uploads'));
+
+
 
 app.use("/category", categoryRoute);
 app.use("/unit", unitOfMeasureRoute);
 app.use("/product", productRoute);
+app.use("/user", userRoute);
+app.use("/cart", cartRoute);
+
 
 app.get("/", async (req, res) => {
+
   return res.status(404).json({ message: "Error page not Found 404" });
 });
 mongoose
